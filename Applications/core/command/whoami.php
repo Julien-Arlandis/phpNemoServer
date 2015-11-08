e2e8d1b96975a12b230d5b6e06995036dab27c0d
Julien Arlandis <julien.arlandis@gmail.com>
2015-11-08 21:08:08 +0100

    update whoami

--- Applications/core/command/whoami.php
+++ Applications/core/command/whoami.php
@@ -5,6 +5,7 @@ if( $this->id )
 	$obj = $this->mongo->user->findOne( array('UserID' => $this->id) );
 	$this->reponse{'code'} = "200";
 	$this->reponse{'body'} = array("FromName"=>$obj{'FromName'}, "FromMail"=>$obj{'FromMail'}, "ReplyTo"=>$obj{'ReplyTo'}, "UserID"=>$this->userid, "email"=>$obj{'email'}, "privilege"=>$this->privilege, "Session"=>$this->session, "hashkey"=>$obj{'hashkey'});
+	$this->reponse{'server'} = $this->config{'domain'};
 }
 else
 {
