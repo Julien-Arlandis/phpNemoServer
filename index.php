97e5f94080390ccd33318a07f2aa2a88064f9931
Julien Arlandis <julien.arlandis@gmail.com>
2016-11-19 16:45:34 +0100

    Update index.php

--- index.php
+++ index.php
@@ -23,6 +23,7 @@ This file is part of PhpNemoServer.
 
 error_reporting( ~E_NOTICE );
 header( "Cache-Control: no-cache, must-revalidate" );
+define('serverVersion', '0.93.1');
 require_once( __DIR__."/Applications/core/lib/class.jntp.php" );
 
 if( !file_exists( __DIR__ . '/conf/config.php' ) ) { require_once( __DIR__."/install.php" ); die(); }
