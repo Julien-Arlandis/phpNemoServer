92f12269cfff0a7691c9f930b2a6ac0efd57bf84
Julien Arlandis <julien.arlandis@gmail.com>
2016-11-19 19:45:42 +0100

    Update decrypt.php

--- Applications/NemoAdmin/command/decrypt.php
+++ Applications/NemoAdmin/command/decrypt.php
@@ -6,7 +6,7 @@ if($jntp->privilege == 'admin' || $jntp->privilege == 'moderator')
 	$packet = $jntp->getPacket( array('Jid'=>$jntp->param{'Jid'}) );
 	$data = $packet{'Data'}{'Media'}[0]{'data'};
 	$keyCrypt = $packet{'Data'}{'Media'}[0]{'KeyAES256'};
-	if (!$privateKey = openssl_get_privatekey(PRIVATE_KEY)) die('Loading Private Key failed');
+	if (!$privateKey = openssl_get_privatekey($jntp->config{'privateKey'})) die('Loading Private Key failed');
 	openssl_private_decrypt(base64_decode($keyCrypt), $key_iv, $privateKey);
 
 	$packet = $jntp->decryptAES256($data, $key_iv);
