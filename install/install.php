c64dbb0679cb947e16492d9085b7168b45fae438
Julien <julien@ns385005.ip-46-105-124.eu>
2016-11-20 15:23:38 +0100

    install

--- install/install.php
+++ install/install.php
@@ -52,8 +52,6 @@ if( !is_writable( __DIR__ . '/../conf'))
 	echo "<strong>chmod o+w jntp/conf</strong><p>";
 }
 
-if($die) die();
-
 if(isset($_GET['php_path']))
 {
 	$php_path = $_GET['php_path'];
@@ -65,8 +63,10 @@ if(isset($_GET['php_path']))
 		flush();
 	}
 	fclose($fp);
-	exit();
+	$die = true;
 }
+
+if($die) die();
 
 $config = array(
     "private_key_bits" => 1024,
@@ -84,109 +84,8 @@ $privateKey = (substr($privateKey, -1, 1) == "\n") ? substr($privateKey, 0, -1) 
 
 if (!isset($_POST['action']) )
 {
-?>
-<!DOCTYPE HTML>
-<html>
-<head>
-<title>Installation de PHP Nemo Server <?=SERVER_VERSION?></title>
-<script src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
-<style>
 
-textarea, .champ, input {
-	font-family: monospace;
-	white-space: pre;
-}
 
-.champ {
-	display: block;
-	float: left;
-	width: 350px;
-}
-
-input[type=text], input[type=password] {
-	width: 280px;
-}
-</style>
-<meta charset="UTF-8">
-</head>
-
-<body>
-
-<?php
-if(php_sapi_name() == 'cli')
-{
-?>
-<h1>CHECK MODE PHP CLI</h1>
-<?=checkModules();?>
-</body>
-</html>
-<?php
-die();
-}
-?>
-
-<h1>Installation de PHP Nemo Server <?=SERVER_VERSION?></h1>
-
-<?=checkModules();?>
-
-<form action="" method="post" enctype="multipart/form-data">
-
-<h2> Configuration générale </h2>
-<span class="champ">[DB_NAME] Nom de la base de données : </span>
-<input name="DB_NAME" type="text" value="nemonews">
-<input name="DEL_DB" type="checkbox" value="1"> Supprimer la base si existante.
-<br>
-<span class="champ">[PHP_PATH] Chemin de l'interpréteur php CLI : </span>
-<input id="PHP_PATH" name="PHP_PATH" type="text" value="/usr/bin/php"><button id="check_php_path" type="button">check</button>
-<br>
-<span class="champ">[PUBLIC_KEY] Clé publique du serveur : </span>
-<textarea id="PUBLIC_KEY" name="PUBLIC_KEY" cols="64" rows="6"><?=$publicKey?></textarea>
-<br>
-<span class="champ">[PRIVATE_KEY] Clé privée du serveur : </span>
-<textarea id="PRIVATE_KEY" name="PRIVATE_KEY" cols="64" rows="16"><?=$privateKey?></textarea>
-<br>
-
-<h2> Compte administrateur</h2>
-<span class="champ">User : </span>
-<input disabled id="user_checked" name="USER" type="text" value="<?='admin'?>">
-<input id="check_add_user" name="ADD_ADMIN" type="checkbox"> Ajouter l'utilisateur dans la base.
-<br>
-<span class="champ">Password : </span>
-<input disabled id="PASSWORD" name="PASSWORD" type="text" value="<?=substr(str_shuffle("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"),0,8);?>">
-<br>
-<span class="champ">Email: </span>
-<input disabled id="EMAIL" name="EMAIL" type="text" value="<?='newsmaster@'.$_SERVER['SERVER_NAME']?>">
-<br><br>
-Le fichier /jntp/conf/config.php va être crée, vous devrez le modifier ultérieurement pour paramétrer les feeds et pour activer les logs.
-<br>
-<input name="action" type="submit" value="Installer">
-</form>
-
-<br><br>
-<script>
-$(document).ready(function() {
-  
-	$('#check_php_path').click(function() {
-		window.open('/jntp/?php_path='+$('#PHP_PATH').val());
-	});
-	
-	$('#check_add_user').change(function() {
-	  
-	  if( this.checked ) {
-		  $( "#user_checked, #PASSWORD, #EMAIL" ).prop( "disabled", false );
-	  }else{
-	    $( "#user_checked, #PASSWORD, #EMAIL" ).prop( "disabled", true );
-	  }
-	  
-	});
-	
-})
-</script>
-
-</body>
-</html>
-
-<?php
 die();
 }
 else
@@ -252,28 +151,14 @@ if(isset($_POST['ADD_ADMIN']))
 $jntp->privilege = 'admin';
 $jntp->exec('["synchronizeNewsgroup"]');
 
+
+echo JNTP::getTpl(__DIR__."/tpl/install_valid.tpl",
+			array(
+				"server_version" => SERVER_VERSION,
+				"dbName" => $jntp->config{'dbName'},
+				"info" => $jntp->reponse{'info'}
+			     )
+	);
+
 }
 ?>
-
-<!DOCTYPE HTML>
-<html>
-<head>
-<title>Installation de PHP Nemo Server (version <?=$server_version?>)</title>
-<meta charset="UTF-8">
-</head>
-<body>
-
-<p>
-Installation de PHP Nemo Server (version <?=$server_version?>) terminée.
-<p>
-La base <?=$jntp->config{'dbName'}?> a bien été crée.
-<p>
-<?=$jntp->reponse{'info'};?>
-<p>
-Vous pouvez désormais configurez les feeds dans le fichier conf/general.json.
-<p>
-Vous devez supprimer le fichier sleep pour continuer.
-<p>
-<strong>rm jntp/sleep</strong>
-</body>
-</html>
