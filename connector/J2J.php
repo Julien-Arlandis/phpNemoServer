8af58a2900618853633f5f03251c186d6bd8996e
Julien Arlandis <julienarlandis@iMac.local>
2017-04-10 11:26:29 +0200

    or your changes. Lines starting

--- connector/J2J.php
+++ connector/J2J.php
@@ -60,6 +60,8 @@ function J2_($server, $jid, $dataid, $datatype)
 
 if(count($argv)>1) 
 {
+
+	
 	if (!$jntp) $jntp = new JNTP(false);
 	require_once(__DIR__."/../Applications/core/lib/class.jntp.php");
 	J2_($argv[1], $argv[2], $argv[3], $argv[4]);
