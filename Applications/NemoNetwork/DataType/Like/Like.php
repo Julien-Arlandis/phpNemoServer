d76042a169f164bdc60c00131b5ab99a571b24e9
Julien <julien@ns385005.ip-46-105-124.eu>
2017-04-10 19:11:50 +0200

    like

--- Applications/NemoNetwork/DataType/Like/Like.php
+++ Applications/NemoNetwork/DataType/Like/Like.php
@@ -4,21 +4,20 @@ class DataType
 	var $jidlike;
 	var $total = 0;
 
-	function __construct() 
+	function __construct()
 	{
 	}
 
 	function isValidData()
 	{
-		global $jntp;
-		if(!$jntp->userid) {
-			$jntp->reponse{'info'} = 'Vous devez être connecté pour liker';
+		if(!JNTP::$userid) {
+			JNTP::$reponse{'info'} = 'Vous devez être connecté pour liker';
 			return false;
 		}
-		$this->total = $jntp->mongo->packet->find(array("Data.UserID"=>$jntp->userid, "Data.DataType" => "Like", "Data.DataIDLike" => $jntp->packet{'Data'}{'DataIDLike'}))->count();
+		$this->total = JNTP::$mongo->packet->find(array("Data.UserID"=>JNTP::$userid, "Data.DataType" => "Like", "Data.DataIDLike" => JNTP::$packet{'Data'}{'DataIDLike'}))->count();
 		if($this->total > 0)
 		{
-			$jntp->reponse{'info'} = 'Vous avez déjà liké cet article';
+			JNTP::$reponse{'info'} = 'Vous avez déjà liké cet article';
 			return false;
 		}
 		else
@@ -29,31 +28,28 @@ class DataType
 
 	function forgeData()
 	{
-		global $jntp;
-		$jntp->packet{'Data'}{'DataID'} = "";
-		$jntp->packet{'Data'}{'InjectionDate'} = date("Y-m-d")."T".date("H:i:s")."Z";
-		$jntp->packet{'Data'}{'OriginServer'} = $jntp->config{'domain'};
-		$jntp->packet{'Data'}{'Organization'} = $jntp->config{'organization'};
-		$jntp->packet{'Data'}{'Browser'} = $_SERVER['HTTP_USER_AGENT'];
-		$jntp->packet{'Data'}{'PostingHost'} = sha1($_SERVER['REMOTE_ADDR']);
-		$jntp->packet{'Data'}{'ComplaintsTo'} = $jntp->config{'administrator'};
-		$jntp->packet{'Data'}{'ProtocolVersion'} = $jntp->config{'protocolVersion'};
-		$jntp->packet{'Data'}{'Server'} = "PhpNemoServer/".$jntp->config{'serverVersion'};
-		$jntp->packet{'Data'}{'UserID'} = $jntp->userid;
+		JNTP::$packet{'Data'}{'DataID'} = "";
+		JNTP::$packet{'Data'}{'InjectionDate'} = date("Y-m-d")."T".date("H:i:s")."Z";
+		JNTP::$packet{'Data'}{'OriginServer'} =JNTP::$config{'domain'};
+		JNTP::$packet{'Data'}{'Organization'} =JNTP::$config{'organization'};
+		JNTP::$packet{'Data'}{'Browser'} = $_SERVER['HTTP_USER_AGENT'];
+		JNTP::$packet{'Data'}{'PostingHost'} = sha1($_SERVER['REMOTE_ADDR']);
+		JNTP::$packet{'Data'}{'ComplaintsTo'} =JNTP::$config{'administrator'};
+		JNTP::$packet{'Data'}{'ProtocolVersion'} =JNTP::$config{'protocolVersion'};
+		JNTP::$packet{'Data'}{'Server'} = "PhpNemoServer/".JNTP::$config{'serverVersion'};
+		JNTP::$packet{'Data'}{'UserID'} = JNTP::$userid;
 	}
 
 	function beforeInsertion()
 	{
-		global $jntp;
 		return true;
 	}
 
 	function afterInsertion()
 	{
-		global $jntp;
-		$jntp->mongo->packet->update( array("Data.DataID"=>$jntp->packet{'Data'}{'DataIDLike'}), array( '$inc' => array("Meta.Like"=>1)) );
+		JNTP::$mongo->packet->update( array("Data.DataID"=>JNTP::$packet{'Data'}{'DataIDLike'}), array( '$inc' => array("Meta.Like"=>1)) );
 		// Diffuse le paquet sur le réseau
-		$jntp->superDiffuse();
+		JNTP::superDiffuse();
 		return true;
 	}
 }
