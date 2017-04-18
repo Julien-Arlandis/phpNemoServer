a844706fa96741ce0533153a31de2b01ce90f7e2
Julien Arlandis <julien.arlandis@gmail.com>
2017-04-18 08:11:04 +0200

    Update class.jntp.php

--- lib/class.jntp.php
+++ lib/class.jntp.php
@@ -53,11 +53,19 @@ class JNTP
 		self::$mongo = $m->selectDB(self::$config{'dbName'});
 	}
 
-	static function go($application, $cmd)
+	static function go()
 	{
-		require_once(__DIR__.'/../Applications/'.$application.'/lib/class.app.php');
-		self::$app = new $application();
-		require_once(__DIR__.'/../Applications/'.$application.'/command/'.$cmd.'.php');
+		if ($application = self::$commandByApplication[self::$command])
+		{
+			require_once(__DIR__.'/../Applications/'.$application.'/lib/class.app.php');
+			self::$app = new $application();
+			require_once(__DIR__.'/../Applications/'.$application.'/command/'.self::$command.'.php');
+		}
+		else
+		{
+			self::$reponse{'code'} = "500";
+			self::$reponse{'info'} = "Command not found, [".self::$command."]";
+		}
 	}
 	
 	// Execute une commande JNTP sur le présent serveur ou sur un serveur distant
@@ -110,15 +118,7 @@ class JNTP
 			self::$reponse{'info'} = "Bad Syntax, type help command";
 			self::send();
 		}
-		if ($application = self::$commandByApplication[self::$command])
-		{
-			self::go($application, self::$command);
-		}
-		else
-		{
-			self::$reponse{'code'} = "500";
-			self::$reponse{'info'} = "Command not found, [".self::$command."]";
-		}
+		self::go();
 	}
 
 	// Retourne le résultat de la requête et stoppe le script
