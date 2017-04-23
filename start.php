b7b6fabe77b2ed6e9b4b7d86636942c597e6f45b
Julien Arlandis <julien.arlandis@gmail.com>
2017-04-23 09:15:26 +0200

    Update start.php

--- start.php
+++ start.php
@@ -2,7 +2,7 @@
 
 define('SERVER_VERSION', '0.94.5');
 require_once( __DIR__."/lib/class.jntp.php" );
-require_once(__DIR__."/class.tools.php");
+require_once(__DIR__."/lib/class.tools.php");
 
 if( !file_exists( __DIR__ . '/conf/config.json' ) ) { require_once( __DIR__."/install/install.php" ); die(); }
 if( file_exists( __DIR__ . '/sleep' ) ) { die( 'You must remove sleep file to continue<br><strong>rm jntp/sleep</strong>' ); }
